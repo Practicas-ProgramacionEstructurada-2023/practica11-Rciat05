8ab6c2568faf2cb2f72c85e53bfc64aac3d7a642
Rciat05 <rciglesiaa05@gmail.com>
2023-10-28 14:54:57 -0600

    practica 11

---
+++ U20230727.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            
+        }
+
+        static int[] numeros = new int[5];
+
+        static int cantidadNumeros = 0; 
+
+        static void IngresarNumeros(){
+            Console.WriteLine("\n Ingresa un número hasta 5 (ingresa -1 para finalizar)");
+            cantidadNumeros = 0;
+
+            while (cantidadNumeros < numeros.Length)
+            {
+                Console.Write("Número #{0}: ", cantidadNumeros + 1);
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero == -1);
+                    break;
+
+                numeros[cantidadNumeros]= numero;    
+                cantidadNumeros++;
+            }
+
+        }
+
+        static int CalcularSuma {
+            
+        }
+
+    }
+}
